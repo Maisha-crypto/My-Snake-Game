599902ab994d2f25fd5856a549debb1a8903bbdc
Maisha-crypto <maishasambopape@gmail.com>
2023-07-11 08:24:07 +0200

    Add the snake control functions, and making variables for constants

--- snake.py
+++ snake.py
@@ -2,11 +2,17 @@ from turtle import Turtle
 
 STARTING_POSITION = [(0,0),(-20,0),(-40,0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
 
 class Snake:
     def __init__ (self):
         self.segments = []
         self.create_snake()
+        self.HEAD_OF_SNAKE = self.segments[0]
         
     def create_snake(self):
 
@@ -22,13 +28,20 @@ class Snake:
             x = self.segments[seg_num-1].xcor()
             y = self.segments[seg_num-1].ycor()
             self.segments[seg_num].goto(x,y)
-        self.segments[0].forward(MOVE_DISTANCE)
+        self.HEAD_OF_SNAKE.forward(MOVE_DISTANCE)
     
     def up(self):
-        self.segments[0].setheading(90)
+        if self.HEAD_OF_SNAKE.heading() != DOWN:
+            self.HEAD_OF_SNAKE.setheading(UP)
+
     def down(self):
-        self.segments[0].setheading(270)
+        if self.HEAD_OF_SNAKE.heading() != UP:
+            self.HEAD_OF_SNAKE.setheading(DOWN)
+
     def left(self):
-        self.segments[0].setheading(180)
+        if self.HEAD_OF_SNAKE.heading() != RIGHT:
+            self.HEAD_OF_SNAKE.setheading(LEFT)
+
     def right(self):
-        self.segments[0].setheading(0)
+        if self.HEAD_OF_SNAKE.heading() != LEFT:
+            self.HEAD_OF_SNAKE.setheading(RIGHT)
