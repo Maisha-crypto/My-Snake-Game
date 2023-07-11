0c1ef58bc29ffe6e1e11eba86062338e6aefa7b2
Maisha-crypto <maishasambopape@gmail.com>
2023-07-11 08:46:04 +0200

    Add description for the functions

--- snake.py
+++ snake.py
@@ -10,12 +10,13 @@ RIGHT = 0
 
 class Snake:
     def __init__ (self):
+        '''This is a default constructor'''
         self.segments = []
         self.create_snake()
         self.HEAD_OF_SNAKE = self.segments[0]
         
     def create_snake(self):
-
+        '''This method creates the initial snake, using 3 square segments.'''
         for position in STARTING_POSITION:
             segment = Turtle(shape='square')
             segment.color('white')
@@ -24,6 +25,7 @@ class Snake:
             self.segments.append(segment)
 
     def move(self):
+        '''This method moves the created segments one at a time '''
         for seg_num in range(len(self.segments)-1, 0, -1):
             x = self.segments[seg_num-1].xcor()
             y = self.segments[seg_num-1].ycor()
@@ -31,17 +33,21 @@ class Snake:
         self.HEAD_OF_SNAKE.forward(MOVE_DISTANCE)
     
     def up(self):
+        '''This method turns the first segment up, if the snake is no already going up.'''
         if self.HEAD_OF_SNAKE.heading() != DOWN:
             self.HEAD_OF_SNAKE.setheading(UP)
 
     def down(self):
+        '''This method turns the first segment down, if the snake is not already going down.'''
         if self.HEAD_OF_SNAKE.heading() != UP:
             self.HEAD_OF_SNAKE.setheading(DOWN)
 
     def left(self):
+        '''This method turns the first segment left, if the snake is not already going right.'''
         if self.HEAD_OF_SNAKE.heading() != RIGHT:
             self.HEAD_OF_SNAKE.setheading(LEFT)
 
     def right(self):
+        '''This method turns the first segment right, if the snake is not already going left.'''
         if self.HEAD_OF_SNAKE.heading() != LEFT:
             self.HEAD_OF_SNAKE.setheading(RIGHT)
