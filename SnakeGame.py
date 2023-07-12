5c3ef4619fd0f83b2650fae28b2e2b141389d608
Maisha-crypto <maishasambopape@gmail.com>
2023-07-12 08:27:24 +0200

    Add detection with wall, and detection with tail functionality

--- SnakeGame.py
+++ SnakeGame.py
@@ -2,6 +2,7 @@ from turtle import Turtle, Screen
 import time
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 
 # initialisation and setup of the screen
 screen = Screen()
@@ -17,6 +18,9 @@ snake.create_snake()    # Creating the snake
 # Creating an instance of the Food class
 food = Food()
 
+# Creating an instance of the Food class
+score = Scoreboard()
+
 isGameOn = True
 
 # initialisation of the screen listen functionality, and setup of the control functions
@@ -25,7 +29,6 @@ screen.onkey(snake.up, "Up")
 screen.onkey(snake.down,'Down')
 screen.onkey(snake.left, 'Left')
 screen.onkey(snake.right, 'Right')
-
 
 while isGameOn:
     screen.update() # updating the screen once all the segments have moved
@@ -36,5 +39,22 @@ while isGameOn:
     # detecting snake collision with food
     if snake.HEAD_OF_SNAKE.distance(food) <= 15:
         food.refresh_food()
+        snake.grow_snake()
+        score.clear()
+        score.update_score()
+
+    # detecting snake collision with wall
+    if snake.HEAD_OF_SNAKE.xcor()>280 or snake.HEAD_OF_SNAKE.xcor()<-280 or snake.HEAD_OF_SNAKE.ycor()>280 or snake.HEAD_OF_SNAKE.ycor()<-280:
+        isGameOn=False 
+        score.game_over()
+
+    # detect collision with tail
+    for seg in snake.segments[1:]: # using slicing to eliminate the first segment when calsulating the distance
+
+        if snake.HEAD_OF_SNAKE.distance(seg) < 10:
+            isGameOn=False
+            score.game_over()
         
+        
+              
 screen.exitonclick()
